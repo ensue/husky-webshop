a73a8675d5bcbcdb4f29180a268649bc4e08a235
jgornik <ens@ram.pl>
2023-03-19 20:52:28 +0100

    create category-item and directory components, adjust  scss styling

--- src/App.js
+++ src/App.js
@@ -1,61 +1,47 @@
-import "./categories.styles.scss";
+import Directory from './components/directory/directory-component.jsx'
+
+
+const categories = [
+  {
+    id: 1,
+    title: "Zestawy",
+    imageUrl:
+      "https://ensue.github.io/husky-webshop/public/resources/img/vacuum1.png",
+    // "https://ensue.github.io/husky-webshop/public/resources/img/zestaw.png",
+  },
+  {
+    id: 2,
+    title: "Akcesoria",
+    imageUrl:
+      "https://ensue.github.io/husky-webshop/public/resources/img/brush.png",
+    // "https://ensue.github.io/husky-webshop/public/resources/img/zestawsp.png",
+  },
+  {
+    id: 3,
+    title: "Montaż",
+    imageUrl:
+      "https://ensue.github.io/husky-webshop/public/resources/img/installation2.png",
+    // "https://ensue.github.io/husky-webshop/public/resources/img/szufelka.png",
+  },
+  {
+    id: 4,
+    title: "Odkurzacze",
+    imageUrl:
+      "https://ensue.github.io/husky-webshop/public/resources/img/units1.png",
+    //"https://ensue.github.io/husky-webshop/public/resources/img/qair.png",
+  },
+  {
+    id: 5,
+    title: "PVC",
+    imageUrl:
+      "https://ensue.github.io/husky-webshop/public/resources/img/pvc2.png",
+    // "https://ensue.github.io/husky-webshop/public/resources/img/pvc.png",
+  },
+];
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Zestawy",
-      imageUrl:
-        "https://ensue.github.io/husky-webshop/public/resources/img/vacuum1.png",
-        // "https://ensue.github.io/husky-webshop/public/resources/img/zestaw.png",
-    },
-    {
-      id: 2,
-      title: "Akcesoria",
-      imageUrl:
-        "https://ensue.github.io/husky-webshop/public/resources/img/brush.png",
-        // "https://ensue.github.io/husky-webshop/public/resources/img/zestawsp.png",
-    },
-    {
-      id: 3,
-      title: "Usługi montażowe",
-      imageUrl:
-        "https://ensue.github.io/husky-webshop/public/resources/img/installation2.png",
-        // "https://ensue.github.io/husky-webshop/public/resources/img/szufelka.png",
-    },
-    {
-      id: 4,
-      title: "Odkurzacze",
-      imageUrl:
-        "https://ensue.github.io/husky-webshop/public/resources/img/units1.png",
-        //"https://ensue.github.io/husky-webshop/public/resources/img/qair.png",
-    },
-    {
-      id: 5,
-      title: "Elementy instalacji",
-      imageUrl:
-        "https://ensue.github.io/husky-webshop/public/resources/img/pvc2.png",
-        // "https://ensue.github.io/husky-webshop/public/resources/img/pvc.png",
-    },
-  ];
-
   return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div
-            className="background-image"
-            style={{
-              backgroundImage: `url(${imageUrl})`,
-            }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Przeglądaj</p>
-          </div>
-        </div>
-      ))}
-    </div>
+<Directory categories={categories}/>
   );
 };
 export default App;
