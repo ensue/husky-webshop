1101a84e67b5abb9db5d94376ea7456e23980dda
jgornik <ens@ram.pl>
2023-03-19 21:10:31 +0100

    add huskylogo.ico, add Oswald font

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,21 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = ({ category: { imageUrl, title } }) => (
+  <div className="category-container">
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Przeglądaj</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default CategoryItem;
