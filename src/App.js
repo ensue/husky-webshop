2edc2dfba5511b6a5472cb7417480eb890468ac7
jgornik <ens@ram.pl>
2023-03-19 10:44:14 +0100

    add category containers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 const App = () => {
   return (
-    <div>husky webshop</div>
+    <div className="categories-container">
+      <div className="category-container">
+        {
+          // <img src="" alt="">
+        }
+        <div className="category-container">
+          <h2>Zestawy</h2>
+          <p>Przeglądaj</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {
+          // <img src="" alt="">
+        }
+        <div className="category-container">
+          <h2>Szczotki</h2>
+          <p>Przeglądaj</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {
+          // <img src="" alt="">
+        }
+        <div className="category-container">
+          <h2>Akcesoria</h2>
+          <p>Przeglądaj</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {
+          // <img src="" alt="">
+        }
+        <div className="category-container">
+          <h2>Odkurzacze</h2>
+          <p>Przeglądaj</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {
+          // <img src="" alt="">
+        }
+        <div className="category-container">
+          <h2>Elementy instalacji</h2>
+          <p>Przeglądaj</p>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
