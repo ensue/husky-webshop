f1a508d3fcc3d21a98546f84ef9bf884265203e2
jgornik <ens@ram.pl>
2023-03-19 17:22:10 +0100

    add commented husky images urls

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
-import './categories.styles.scss'
+import "./categories.styles.scss";
 
 const App = () => {
   const categories = [
     {
       id: 1,
       title: "Zestawy",
+      imageUrl:
+        "https://ensue.github.io/husky-webshop/public/resources/img/vacuum1.png",
+        // "https://ensue.github.io/husky-webshop/public/resources/img/zestaw.png",
     },
     {
       id: 2,
-      title: "Szczotki",
+      title: "Akcesoria",
+      imageUrl:
+        "https://ensue.github.io/husky-webshop/public/resources/img/brush.png",
+        // "https://ensue.github.io/husky-webshop/public/resources/img/zestawsp.png",
     },
     {
       id: 3,
-      title: "Akcesoria",
+      title: "Usługi montażowe",
+      imageUrl:
+        "https://ensue.github.io/husky-webshop/public/resources/img/installation2.png",
+        // "https://ensue.github.io/husky-webshop/public/resources/img/szufelka.png",
     },
     {
       id: 4,
       title: "Odkurzacze",
+      imageUrl:
+        "https://ensue.github.io/husky-webshop/public/resources/img/units1.png",
+        //"https://ensue.github.io/husky-webshop/public/resources/img/qair.png",
     },
     {
       id: 5,
       title: "Elementy instalacji",
+      imageUrl:
+        "https://ensue.github.io/husky-webshop/public/resources/img/pvc2.png",
+        // "https://ensue.github.io/husky-webshop/public/resources/img/pvc.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => (
-      
-        <div key ={id} className="category-container">
-          <div className = ''></div>
+      {categories.map(({ title, id, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Przeglądaj</p>
@@ -38,6 +57,5 @@ const App = () => {
       ))}
     </div>
   );
-
 };
 export default App;
