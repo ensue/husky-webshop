ef9517b53c6f4a52494befb3fd29710962e042a8
jgornik <ens@ram.pl>
2023-03-20 19:06:12 +0100

    add sign-in component, add navbar sign-in link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component.jsx";
-import Navigation from './components/navigation/navigation.component.jsx'
+import Navigation from "./components/navigation/navigation.component.jsx";
+import SignIn from "./components/sign-in/sign-in.component.jsx";
 
 const Shop = () => {
   return <h1>shop page</h1>;
 };
+
+
+
 
 const App = () => {
   return (
@@ -12,6 +16,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/sklep" element={<Shop />} />
+        <Route path="/login" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -2,19 +2,22 @@ import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 import "./navigation.styles.scss";
 
-import {ReactComponent as HuskyLogo} from '../../assets/logo_husky.svg';
+import { ReactComponent as HuskyLogo } from "../../assets/logo_husky.svg";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <HuskyLogo className='logo'/>
+          <HuskyLogo className="logo" />
         </Link>
 
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SKLEP
+          </Link>
+          <Link className="nav-link" to="/login">
+            LOGIN
           </Link>
         </div>
       </div>
