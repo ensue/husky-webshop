4e7604c6de93db2c780834cbc3bee2d23cb464a0
jgornik <ens@ram.pl>
2023-03-20 20:58:00 +0100

    styling no borders homepage

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,11 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,9 +19,17 @@ const firebaseConfig = {
   storageBucket: "husky-webshop.appspot.com",
   messagingSenderId: "639056233874",
   appId: "1:639056233874:web:f61f0ed5aa357a9b6ced4b",
-  measurementId: "G-WCVK2WK82Q"
+  measurementId: "G-WCVK2WK82Q",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
