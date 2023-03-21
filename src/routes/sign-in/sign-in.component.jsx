b8ff6c48e0b3a3977c5b8ff7e331c10abd6cbf35
jgornik <ens@ram.pl>
2023-03-21 17:12:12 +0100

    make password confirmation check

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,6 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
 import {
   auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
@@ -19,9 +15,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
+      
     </div>
   );
 };
