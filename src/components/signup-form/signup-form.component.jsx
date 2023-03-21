6a3366d46b370dccda2d8ca8489999af655f6e04
jgornik <ens@ram.pl>
2023-03-21 12:26:41 +0100

    add form input handler

--- src/components/signup-form/signup-form.component.jsx
+++ src/components/signup-form/signup-form.component.jsx
@@ -11,19 +11,22 @@ const SignUpForm = () => {
     const [formFields,setFormFields] = useState(defaultFormFields);
     const {displayName, email, password, confirmPassword} = formFields;
 
+    const handleChange = (event)=>{
+
+    };
 
   return (
     <div>
       <h1>Zarejestruj</h1>
       <form onSubmit={() => {}}>
         <label>Nazwa użytkownika</label>
-        <input type="text" required />
+        <input type="text" required onChange={handleChange}/>
         <label>Email</label>
-        <input type="email" required />
+        <input type="email" required onChange={handleChange}/>
         <label>Hasło</label>
-        <input type="password" required />
+        <input type="password" required onChange={handleChange}/>
         <label>Potwierdź hasło</label>
-        <input type="password" required />
+        <input type="password" required onChange={handleChange}/>
         <button type="submit">Zarejestruj</button>
       </form>
     </div>
