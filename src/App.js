d8eae5f016938c41ca836e85754c3e518dd60a0e
jgornik <ens@ram.pl>
2023-03-24 20:58:19 +0100

    create shop component, create shop-data.json, link app to shop component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component.jsx";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import Authentication from "./routes/authentication/authentication.component.jsx";
+import Shop from "./routes/shop/shop.component.jsx";
 
-const Shop = () => {
-  return <h1>shop page</h1>;
-};
+
 
 
 
@@ -15,8 +14,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/auth" element={<Authentication />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
