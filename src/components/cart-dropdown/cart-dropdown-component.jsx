0713bf68ff4b765ad1b74604b33bca3ba1b570d7
jgornik <ens@ram.pl>
2023-03-25 22:28:23 +0100

    create cart-item component, create logic for adding items to cart, include cart-item component in cart-dropdown component

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -1,16 +1,21 @@
-import './cart-dropdown.styles.scss';
-import Button from '../button/button.component';
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+import "./cart-dropdown.styles.scss";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () =>{
-    return(
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                <Button>
-                    KOSZYK
-                </Button>
-            </div>
-        </div>
-    )
-}
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+        <Button>KOSZYK</Button>
+      </div>
+    </div>
+  );
+};
 
 export default CartDropdown;
--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -2,9 +2,8 @@ import { createContext, useState, useEffect } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
-  signOutUser,
 } from "../utils/firebase/firebase.utils";
-//value to access globally
+
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
@@ -13,11 +12,10 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
-  signOutUser();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      if(user){
+      if (user) {
         createUserDocumentFromAuth(user);
       }
       setCurrentUser(user);
@@ -25,5 +23,6 @@ export const UserProvider = ({ children }) => {
 
     return unsubscribe;
   }, []);
+
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
