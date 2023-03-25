91ab9569d929c64b261c3dd1fb5ac32df377fd53
jgornik <ens@ram.pl>
2023-03-25 16:40:02 +0100

    create product-card component that uses products-context, link it to shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,18 @@
-import SHOP_DATA from '../../shop-data.json'
+import { useContext } from "react";
+import { ProductsContext } from "../../context/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
 
 const Shop = () => {
-return(
+  const { products } = useContext(ProductsContext);
+  return (
     <div>
-        {SHOP_DATA.map(({id, name})=>(
-            <div key={id}>
-                <h1>
-                {name}
-                </h1>
-            </div>
-        ))}
+      {products.map(({ id, name }) => (
+        <div key={id}>
+          <h1>{name}</h1>
+        </div>
+      ))}
     </div>
-)
-}
+  );
+};
 
 export default Shop;
