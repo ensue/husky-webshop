e4dd131198a5bcdd3e35b4b5cb16ab879621bc04
jgornik <ens@ram.pl>
2023-03-28 17:49:01 +0200

    migrate cart-icon to styled components

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -8,16 +8,17 @@ import {
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
-  width: 240px;
-  height: 340px;
+  width: 300px;
+  height: 300px;
   display: flex;
   flex-direction: column;
-  padding: 20px;
+  padding: 10px;
   border: 1px solid black;
   background-color: white;
-  top: 90px;
-  right: 40px;
+  top: 80px;
+  right: 20px;
   z-index: 5;
+  
 
   ${BaseButton},
   ${GoogleSignInButton},
@@ -28,14 +29,14 @@ export const CartDropdownContainer = styled.div`
 
 export const EmptyMessage = styled.span`
   font-size: 18px;
-  margin: 50px auto;
+  margin: 30px auto;
 `;
 
 export const CartItems = styled.div`
   height: 240px;
   display: flex;
   flex-direction: column;
-  overflow: scroll;
+  overflow: auto;
 `;
 
 
