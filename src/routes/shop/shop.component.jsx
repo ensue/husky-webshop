a675b733d94d31d70de1358bd0e549a106339f73
jgornik <ens@ram.pl>
2023-04-03 12:36:33 +0200

    add fetchCategoriesAsync function in categories.action and link it to useEffect in shop.component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,15 +3,13 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/categories.action";
+import { fetchCategoriesAsync } from "../../store/categories/categories.action";
 
 const Shop = () => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
+    const getCategoriesMap = () => {
+      dispatch(fetchCategoriesAsync());
     };
     getCategoriesMap();
   }, []);
