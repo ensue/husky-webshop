5d263cd4282a0aa44053d64e42427883e8f06702
jgornik <ens@ram.pl>
2023-04-04 17:53:34 +0200

    create getCurrentUser async function in firebase.utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -117,3 +117,16 @@ export const signOutUser = async () => await signOut(auth);
 export const onAuthStateChangedListener = (callback) => {
   onAuthStateChanged(auth, callback);
 };
+
+//redux-saga based user authentication
+
+export const getCurrentUser = () => {
+  return new Promise((resolve,reject)=>{
+    const unsubscribe = onAuthStateChanged(
+      auth,(userAuth)=>{
+        unsubscribe();
+      },
+      reject
+    )
+  })
+}
