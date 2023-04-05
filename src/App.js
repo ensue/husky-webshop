4828b092f3530dfaab31501f71cf0e1fc340ac4a
jgornik <ens@ram.pl>
2023-04-05 17:29:27 +0200

    rename shop-data.js, add addCollectionAndDocuments in app.js for importing SHOP_DATA to Firestore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
-import { checkUserSession } from './store/user/user.action';
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
+import { checkUserSession } from "./store/user/user.action";
+
+import { addCollectionAndDocuments } from "./utils/firebase/firebase.utils";
+import SHOP_DATA from "./shop";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,6 +19,10 @@ const App = () => {
   useEffect(() => {
     dispatch(checkUserSession());
   }, []);
+
+  // useEffect(() => {;
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
 
   return (
     <Routes>
