03b93b8740bedbefe2e5fcf8839aa01602a29778
jgornik <ens@ram.pl>
2023-04-14 13:30:39 +0200

    create dispatch for closing dropdown cart before redirecting to checkout upon clicking the checkout button in dropdown cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,19 +1,22 @@
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
-import { selectCartItems } from "../../store/cart/cart.selector";
+import { selectCartItems, selectIsCartOpen } from "../../store/cart/cart.selector";
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
 } from "./cart-dropdown.styles";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const goToCheckoutHandler = () => {
+    dispatch(setIsCartOpen(false));
     navigate("/koszyk");
   };
 
