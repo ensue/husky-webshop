0fcad2020ebab0d724517f64639a5a28ef0d11a1
jgornik <ens@ram.pl>
2023-04-21 11:11:11 +0200

    type category.actions

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,42 +1,37 @@
-import {
-  CATEGORIES_ACTION_TYPES,
-  Category,
-  CategoryItem,
-} from "./category.types";
+import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
+
 import {
   createAction,
   Action,
+  withMatcher,
   ActionWithPayload,
-} from "../../utils/reducer/reducer.utils";
+} from '../../utils/reducer/reducer.utils';
 
-export type fetchCategoriesStart =
+export type FetchCategoriesStart =
   Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
 
-export type fetchCategoriesSuccess = ActionWithPayload<
+export type FetchCategoriesSuccess = ActionWithPayload<
   CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
   Category[]
 >;
 
-export type fetchCategoriesFailed = ActionWithPayload<
+export type FetchCategoriesFailed = ActionWithPayload<
   CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
   Error
 >;
 
-export type CategoryAction =
-  | fetchCategoriesStart
-  | fetchCategoriesSuccess
-  | fetchCategoriesFailed;
+export const fetchCategoriesStart = withMatcher(() =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+);
 
-export const fetchCategoriesStart = (): fetchCategoriesStart =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesSuccess = withMatcher(
+  (categoriesArray: Category[]) =>
+    createAction(
+      CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+      categoriesArray
+    )
+);
 
-export const fetchCategoriesSuccess = (
-  categoriesArray: Category[]
-): fetchCategoriesSuccess =>
-  createAction(
-    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
-    categoriesArray
-  );
-
-export const fetchCategoriesFailed = (error: Error): fetchCategoriesFailed =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher((error: Error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+);
