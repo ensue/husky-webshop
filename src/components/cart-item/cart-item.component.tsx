9a370a177463cd80ddfb6702d8aecaac3c4bad7c
jgornik <ens@ram.pl>
2023-04-25 16:41:39 +0200

    memoize cartItem in cart-item.component

--- src/components/cart-item/cart-item.component.tsx
+++ src/components/cart-item/cart-item.component.tsx
@@ -1,14 +1,14 @@
-import { FC } from 'react';
+import { FC, memo } from "react";
 
-import { CartItemContainer, ItemDetails } from './cart-item.styles';
+import { CartItemContainer, ItemDetails } from "./cart-item.styles";
 
-import { CartItem as TCartItem } from '../../store/cart/cart.types';
+import { CartItem as TCartItem } from "../../store/cart/cart.types";
 
 type CartItemProps = {
   cartItem: TCartItem;
 };
 
-const CartItem: FC<CartItemProps> = ({ cartItem }) => {
+const CartItem: FC<CartItemProps> = memo(({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <CartItemContainer>
@@ -21,6 +21,6 @@ const CartItem: FC<CartItemProps> = ({ cartItem }) => {
       </ItemDetails>
     </CartItemContainer>
   );
-};
+});
 
 export default CartItem;
