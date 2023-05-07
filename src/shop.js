b615857d2be7db723d1e48da4807ab9409b50196
jgornik <ens@ram.pl>
2023-05-07 14:32:31 +0200

    add central units and kits photos and info to assets and shop.js

--- src/shop.js
+++ src/shop.js
@@ -1,233 +1,297 @@
 const SHOP_DATA = [
-    {
-      title: 'Jednostki Centralne',
-      items: [
+  {
+    title: "Jednostki Centralne",
+    items: [
+      {
+        id: 101,
 
-        {
-          id: 1,
-          name: "Jednostka Centralna Q Air Cyklon, 1850W",
-          symbol: "QAC-UNIT",
-          imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_qair.png",
-          price: 3700
-        },
-        {
-          id: 2,
-          name: "Jednostka Centralna Q Air Cyklon, 1850W",
-          symbol: "QAC-UNIT",
-          imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_qair.png",
-          price: 3600
-        },
-        {
-          id: 3,
-          name: "Jednostka Centralna Q Air Cyklon, 1850W",
-          symbol: "QAC-UNIT",
-          imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_qair.png",
-          price: 3500
-        },
-        {
-          id: 4,
-          name: "Jednostka Centralna Q Air Cyklon, 1850W",
-          symbol: "QAC-UNIT",
-          imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_qair.png",
-          price: 3400
-        },
-        {
-          id: 5,
-          name: "Jednostka Centralna Q Air Cyklon, 1850W",
-          symbol: "QAC-UNIT",
-          imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_qair.png",
-          price: 3300
-        },
-        {
-          id: 6,
-          name: "Jednostka Centralna Q Air Cyklon, 1850W",
-          symbol: "QAC-UNIT",
-          imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_qair.png",
-          price: 3200
-        },
-        {
-          id: 7,
-          name: "Jednostka Centralna Q Air Cyklon, 1850W",
-          symbol: "QAC-UNIT",
-          imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_qair.png",
-          price: 3600
-        },
-      ],
-    },
-    {
-      title: 'Przewody powietrzne',
-      items: [
-        {
-          id: 10,
-          name: 'Zestaw elementów instalacji',
-          symbol: 'H807-306',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg',
-          price: 500,
-        },
-        {
-          id: 11,
-          name: 'Szufelka przypodłogowa',
-          symbol: 'SZU-306',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_szu.png',
-          price: 500,
-        },
-        {
-          id: 12,
-          name: 'Zestaw elementów instalacji',
-          symbol: 'H807-306',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg',
-          price: 500,
-        },
-        {
-          id: 13,
-          name: 'Zestaw elementów instalacji',
-          symbol: 'H807-306',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg',
-          price: 500,
-        },
-        {
-          id: 14,
-          name: 'Zestaw elementów instalacji',
-          symbol: 'H807-306',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg',
-          price: 500,
-        },
-        {
-          id: 15,
-          name: 'Zestaw elementów instalacji',
-          symbol: 'H807-306',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg',
-          price: 500,
-        },
-        {
-          id: 16,
-          name: 'Zestaw elementów instalacji',
-          symbol: 'H807-306',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg',
-          price: 500,
-        },
-        {
-          id: 17,
-          name: 'Zestaw elementów instalacji',
-          symbol: 'H807-306',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg',
-          price: 500,
-        },
-      ],
-    },
-    {
-      title: 'Zestawy',
-      items: [
-        {
-          id: 18,
-          name: 'Zestaw Q Air Cyklon + akcesoria',
-          symbol: 'QAC-KIT',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_kit.png',
-          price: 4100,
-        },
-        {
-          id: 19,
-          name: 'Zestaw Q Air Cyklon + akcesoria',
-          symbol: 'QAC-KIT',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_kit.png',
-          price: 4100,
-        },
-        {
-          id: 20,
-          name: 'Zestaw Q Air Cyklon + akcesoria',
-          symbol: 'QAC-KIT',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_kit.png',
-          price: 4100,
-        },
-        {
-          id: 21,
-          name: 'Zestaw Q Air Cyklon + akcesoria',
-          symbol: 'QAC-KIT',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_kit.png',
-          price: 4100,
-        },
-        {
-          id: 22,
-          name: 'Zestaw Q Air Cyklon + akcesoria',
-          symbol: 'QAC-KIT',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_kit.png',
-          price: 4100,
-        },
-      ],
-    },
-    {
-      title: 'Akcesoria',
-      items: [
-        {
-          id: 23,
-          name: 'Zestaw akcesoriów',
-          symbol: 'PN200',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_acc2.png',
-          price: 700,
-        },
-        {
-          id: 24,
-          name: 'Zestaw akcesoriów',
-          symbol: 'PN200',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_acc2.png',
-          price: 700,
-        },
-        {
-          id: 25,
-          name: 'Zestaw akcesoriów',
-          symbol: 'PN200',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_acc2.png',
-          price: 700,
-        },
-        {
-          id: 26,
-          name: 'Zestaw akcesoriów',
-          symbol: 'PN200',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_acc2.png',
-          price: 700,
-        },
-        {
-          id: 27,
-          name: 'Zestaw akcesoriów',
-          symbol: 'PN200',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_acc2.png',
-          price: 700,
-        },
-        {
-          id: 28,
-          name: 'Zestaw akcesoriów',
-          symbol: 'PN200',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_acc2.png',
-          price: 700,
-        },
-        {
-          id: 29,
-          name: 'Zestaw akcesoriów',
-          symbol: 'PN200',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/or_acc2.png',
-          price: 700,
-        },
-      ],
-    },
-    {
-      title: 'Instalacja',
-      items: [
-        {
-          id: 30,
-          name: 'Projekt instalacji',
-          symbol: 'PROJ00',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/bw_inst.jpg',
-          price: 0,
-        },
-        {
-          id: 31,
-          name: 'Usługa montażu instalacji',
-          symbol: 'INST00',
-          imageUrl: 'https://ensue.github.io/husky-webshop/src/assets/bw_inst2.png',
-          price: 1700,
-        },
-      ],
-    },
-  ];
-  
-  export default SHOP_DATA;
+        name: "Husky Q AIR CYKLON",
+
+        symbol: "JED001",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JED001_qaircyklon/qaircyklon.png",
+
+        price: 2499,
+      },
+
+      {
+        id: 102,
+
+        name: "Husky FLEX odkurzacz centralny",
+
+        symbol: "JED002",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JED002_flex/HFlex_11.jpg",
+
+        price: 4365,
+      },
+
+      {
+        id: 103,
+
+        name: "Husky EVOLUTION odkurzacz centralny",
+
+        symbol: "JED003",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JED003_evo/HEvol_11.jpg",
+        price: 5595,
+      },
+
+      {
+        id: 104,
+
+        name: "Husky WHISPER odkurzacz centralny",
+
+        symbol: "JED004",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JED004_whisp/HWhisp_11.jpg",
+        price: 7058,
+      },
+
+      {
+        id: 105,
+
+        name: "Husky PRO 100 odkurzacz centralny",
+
+        symbol: "JED005",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JED005_pro/HPRO100_11.jpg",
+        price: 6560,
+      },
+
+      {
+        id: 106,
+
+        name: "Husky TITAN odkurzacz centralny",
+
+        symbol: "JED006",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JED006_titan/HTitan_11.jpg",
+        price: 9629,
+      },
+    ],
+  },
+  {
+    title: "Zestawy",
+    items: [
+      {
+        id: 201,
+
+        name: "Husky Q AIR CYKLON odkurzacz centralny z zestawem do sprzątania",
+
+        symbol: "JEDX001",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JEDX001_qaircyklon_kit/jedx001_kit_0.jpg",
+        price: 3199,
+      },
+      {
+        id: 202,
+
+        name: "Husky FLEX odkurzacz centralny z zestawem do sprzątania",
+
+        symbol: "JEDX002",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JEDX002_flex_kit/HFlex_1.jpg",
+        price: 4660,
+      },
+      {
+        id: 203,
+
+        name: "Husky EVOLUTION odkurzacz centralny z zestawem do sprzątania",
+
+        symbol: "JEDX003",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JEDX003_evo_kit/HEvol_1.jpg",
+        price: 5671,
+      },
+      {
+        id: 204,
+
+        name: "Husky WHISPER odkurzacz centralny z zestawem do sprzątania",
+
+        symbol: "JEDX004",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JEDX004_whisp_kit/HWhisp_1.jpg",
+        price: 7427,
+      },
+      {
+        id: 205,
+
+        name: " Husky PRO 100 odkurzacz centralny z zestawem do sprzątania",
+
+        symbol: "JEDX005",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JEDX005_pro_kit/HPRO100_1.jpg",
+        price: 7209,
+      },
+      {
+        id: 206,
+
+        name: " Husky TITAN odkurzacz centralny z zestawem do sprzątania",
+
+        symbol: "JEDX006",
+
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/products/JEDX006_titan_kit/HTitan_1.jpg",
+        price: 9887,
+      },
+    ],
+  },
+
+  {
+    title: "Przewody powietrzne",
+    items: [
+      {
+        id: 301,
+        name: "Zestaw elementów instalacji",
+        symbol: "H807-306",
+        imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg",
+        price: 500,
+      },
+      {
+        id: 302,
+        name: "Szufelka przypodłogowa",
+        symbol: "SZU-306",
+        imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_szu.png",
+        price: 500,
+      },
+      {
+        id: 303,
+        name: "Zestaw elementów instalacji",
+        symbol: "H807-306",
+        imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg",
+        price: 500,
+      },
+      {
+        id: 304,
+        name: "Zestaw elementów instalacji",
+        symbol: "H807-306",
+        imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg",
+        price: 500,
+      },
+      {
+        id: 305,
+        name: "Zestaw elementów instalacji",
+        symbol: "H807-306",
+        imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg",
+        price: 500,
+      },
+      {
+        id: 306,
+        name: "Zestaw elementów instalacji",
+        symbol: "H807-306",
+        imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg",
+        price: 500,
+      },
+      {
+        id: 307,
+        name: "Zestaw elementów instalacji",
+        symbol: "H807-306",
+        imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg",
+        price: 500,
+      },
+      {
+        id: 308,
+        name: "Zestaw elementów instalacji",
+        symbol: "H807-306",
+        imageUrl: "https://ensue.github.io/husky-webshop/src/assets/or_pvc.jpg",
+        price: 500,
+      },
+    ],
+  },
+  {
+    title: "Akcesoria",
+    items: [
+      {
+        id: 401,
+        name: "Zestaw akcesoriów",
+        symbol: "PN200",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/or_acc2.png",
+        price: 700,
+      },
+      {
+        id: 402,
+        name: "Zestaw akcesoriów",
+        symbol: "PN200",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/or_acc2.png",
+        price: 700,
+      },
+      {
+        id: 403,
+        name: "Zestaw akcesoriów",
+        symbol: "PN200",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/or_acc2.png",
+        price: 700,
+      },
+      {
+        id: 404,
+        name: "Zestaw akcesoriów",
+        symbol: "PN200",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/or_acc2.png",
+        price: 700,
+      },
+      {
+        id: 405,
+        name: "Zestaw akcesoriów",
+        symbol: "PN200",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/or_acc2.png",
+        price: 700,
+      },
+      {
+        id: 406,
+        name: "Zestaw akcesoriów",
+        symbol: "PN200",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/or_acc2.png",
+        price: 700,
+      },
+      {
+        id: 407,
+        name: "Zestaw akcesoriów",
+        symbol: "PN200",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/or_acc2.png",
+        price: 700,
+      },
+    ],
+  },
+
+  {
+    title: "Instalacja",
+    items: [
+      {
+        id: 501,
+        name: "Projekt instalacji",
+        symbol: "PROJ00",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/bw_inst.jpg",
+        price: 0,
+      },
+      {
+        id: 502,
+        name: "Usługa montażu instalacji",
+        symbol: "INST00",
+        imageUrl:
+          "https://ensue.github.io/husky-webshop/src/assets/bw_inst2.png",
+        price: 1700,
+      },
+    ],
+  },
+];
+
+export default SHOP_DATA;
