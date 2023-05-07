9aaf1419920d5e7bb82df5d2db13cfc66d64f94f
jgornik <ens@ram.pl>
2023-05-07 20:08:28 +0200

    set isCartOpen as false after navigating to /koszyk from cart-dropdown.component

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -12,13 +13,17 @@ import {
   EmptyMessage,
   CartItems,
 } from './cart-dropdown.styles';
+import { setIsCartOpen } from '../../store/cart/cart.action';
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const goToCheckoutHandler = useCallback(() => {
+    
+    dispatch(setIsCartOpen(false));
     navigate('/koszyk');
+
   }, []);
 
   return (
